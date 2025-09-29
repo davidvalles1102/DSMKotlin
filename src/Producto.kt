d0f6827bcb59d79cbf608846ca0ebb99246652bb
Jonat1994 <jonathanjoseflamenco@gmail.com>
2025-09-28 18:54:21 -0600

    Se agregaron nuevas variables.

--- src/Producto.kt
+++ src/Producto.kt
@@ -1,23 +1,19 @@
 data class Producto(
-    // variable que almacena el nombre del producto.
+    //  almacena el nombre del producto.
     val nombre: String,
 
-    // variable que almacena el precio del producto.
+    // almacena el precio del producto.
     val precio: Double,
 
-    // variable que almacena la cantidad disponible en stock del producto.
+    //  almacena la cantidad disponible en stock del producto.
     var cantidadDisponible: Int
 ) {
 
-    // Sobrescribe el método equals para definir cómo se comparan dos objetos de tipo Producto.
+    // Sobrescribe el metodo equals para definir cómo se comparan dos objetos de tipo Producto.
+
     override fun equals(other: Any?): Boolean {
-        // Verifica si el objeto comparado es el mismo (misma referencia en memoria).
         if (this === other) return true
-
-        // Verifica si el objeto comparado no es de tipo Producto.
         if (other !is Producto) return false
-
-        // Compara los nombres y precios de los productos para determinar si son iguales.
         return nombre == other.nombre && precio == other.precio
     }
 
